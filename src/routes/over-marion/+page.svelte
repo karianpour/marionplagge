29b7f80dd51bab4ac2c4f708bb232ab7712136e9
Remko Bolt <remko@cluebox.org>
2025-02-22 12:13:38 +0000

    Changed text over-marion/index.html

--- src/routes/over-marion/+page.svelte
+++ src/routes/over-marion/+page.svelte
@@ -21,13 +21,77 @@
 				<div class="container">
 					<div class="wsite-section-elements">
 						<div class="paragraph">
-							Ik ben een therapeut met een holistische visie. In 2014 ben ik begonnen met
-							de jaaropleiding Bewegingstherapie voor paarden bij Moravita ISHAS. Daarna
-							heb ik in een aantal jaren tijd mijn Sportmassage licentie 4 gehaald bij
-							Massage Hippique. In 2015 ben ik gestart met de opleiding
-							craniosacraaltherapie voor paarden bij Maureen Rogers. Inmiddels ben ik
-							Level 3 Equine Craniosacral Therapist. In 2021 Ben ik begonnen bij het
-							Milne instituut met Visionary Craniosacral Work®.<br />
+							<p>
+							Wie ben ik?
+							</p><br />
+							<p>
+							Ik ben een paardengek. Vanaf mijn zesde iedere dag op en bij ponies te
+							vinden. Iedere week les op de ponyclub en bijna ieder weekend op wedstrijd.
+							Andere sporten vond ik gewoonweg niet interessant. Toen ik ging studeren
+							moest ik even afscheid nemen van de paarden. Ik voelde een leegte en zodra
+							ik kon zat ik weer in het zadel en pakte ik mijn sport weer vol overgave op.
+							Momenteel hebben wedstrijden plaats gemaakt voor lessen en buitenritten in
+							het bos. Iets waar ik nu samen met mijn man en onze paarden enorm van kan
+							genieten.
+							</p><br />
+							<p>
+							Dieren hebben mij altijd aangetrokken. Van kinds af aan wilde ik dierenarts
+							worden. Maar helaas, ik bleek te slecht te zijn in de exacte vakken. En wat
+							dan, ik wist het niet. Dus daarom maar een 'verstandige studie' gekozen,
+							zodat ik in mijn vrije tijd mij kon omringen met dieren en vooral paarden.
+							Toch bleef het altijd maar knagen en was ik niet tevreden met hoe ik mij
+							leven vorm gaf. Ik besloot een duitse opleiding tot fysiotherapie (in
+							Nederland Bewegingstherapie genoemd) voor paarden te doen, omdat ik wilde
+							weten hoe het paard in elkaar zat en hoe ik daar rijkunstig mee om kon gaan.
+							En dat smaakte naar meer. Ik ging een meerjarige massageopleiding doen.
+							Hier leerde ik uiteindelijk veel meer dan alleen massage; aromatherapie,
+							werken met edelstenen, taping en raindroptherapie. Het kwam allemaal ruim
+							aan bod. Net als meerdere dissecties op de Universiteit voor
+							Diergeneeskunde. 
+							</p><br />
+							<p>
+							En toen kwam craniosacraaltherapie op mijn pad. Tijdens mijn eerste
+							cursusweek had ik geen idee wat ik deed, snapte ik niet wat het
+							craniosacrale systeem was. Maar ik zag wel de resultaten! Die waren groter
+							en indrukwekkender dan die van de massage en de bewegingstherapie. De
+							paarden waren na 1 behandeling al aanzienlijk soepeler en waren rustiger in
+							hun gedrag. Het bleef maar rondspoken in mijn hoofd; wat is dit? Hoe werkt
+							dit? Wat gebeurt er in het paard? En zo is de diepe duik in
+							craniosacraaltherapie voor mij begonnen. En deze duik is nog maar net onder
+							de oppervlakte. Na de paarden ben ik gestart met een humane opleiding tot
+							craniosacraaltherapeut. Hopelijk mag ik in 2025 mijn diploma in ontvangst
+							nemen.
+							</p><br />
+							<p>
+							Paarden zijn voor mij als personen. Ze hebben allemaal hun eigen karakter.
+							Mijn grootste uitdaging en plezier is om met al mijn zintuigen te luisteren
+							naar het verhaal dat ze te vertellen hebben. En ze hebben genoeg te
+							vertellen! Waar hun pijn zit, wat ze bezig houdt, hoe ze hun pijntjes
+							compenseren, wat ze leuk vinden. Tijdens de behandeling komt het echte
+							paard naar boven. Een chagrijnig paard wordt ineens zacht, een afgesloten
+							paard begint een interactie, een strak paard ontspant. De genezing is
+							begonnen en dit verandert de relatie tussen eigenaar en paard. Er is een
+							nieuw perspectief ontstaan voor hen samen. En daar doe ik het voor.
+							</p><br />
+							<p>
+							Ik ben een streber. Dat moet ik toegeven. Ik wil altijd het beste uit mezelf
+							halen en van mezelf kunnen geven. Daarom kies ik voor de beste opleidingen
+							met veel studie-uren. Ik ga bewust op zoek naar de pioniersin hun vakgebied.
+							Zij hebben veel meegemaakt, inclusief fouten, waarvan ik mag leren. Zij zijn
+							een inspiratiebron voor mij. Dit betekent dat ik vele uren in studies
+							investeer en dat ik veel in het buitenland ben om deze mensen te ontmoeten
+							en met hen te werken. Niet de makkelijkste weg, wel de leukste!
+							</p><br />
+							<p>
+							Ik ben een nuchter mens. Met beide benen op de grond. Ik ben opgegroeid en
+							opgeleid met de gedachte dat iets pas bestaat als het wetenschappelijk
+							bewezen is. Ik hecht nog steeds heel veel waarde aan wetenschappelijk
+							onderzoek en hoop dat er nog veel onderzoek zal worden gedaan in de
+							paardenwereld. Dat hebben we echt nodig. Tegelijk heb ik door mijn
+							opleidingen en ervaringen ervaren dat er meer is dan wetenschap. Mijn visie
+							is daarom: Als iets bewezen is, betekent dit dat het bestaat. Als iets níet
+							bewezen is, betekent dit nog niet dat het níet bestaat.
+							</p><br />
 							<br />
 							<strong>2023</strong><br />
 							<a href="https://milneinstitute.com/cranial-4-c4/"
