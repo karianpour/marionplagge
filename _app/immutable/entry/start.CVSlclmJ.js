7f48b9e46e7d8f43d2708a9c61f2695755afb403
Remko Bolt <remko@cluebox.org>
2025-02-22 11:37:40 +0000

    Updates

--- _app/immutable/entry/start.CVSlclmJ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DaZS6JLB.js";export{t as start};
---
+++ _app/immutable/nodes/4.BFUM57gQ.js
@@ -0,0 +1,26 @@
+import{h as d,a as n,t as r}from"../chunks/disclose-version.C0HbHUeE.js";import"../chunks/legacy.naJ-WMj7.js";import{g,$ as l,n as a}from"../chunks/runtime.BEGJMphK.js";import{b as m}from"../chunks/paths.xS8Suj8J.js";import{B as p}from"../chunks/Banner.CEoGquR7.js";var b=r('<meta property="og:url" content="/over-marion"> <meta property="og:title" content="Over Marion Plagge"> <meta property="og:description" content="ik ben craniosacraaltherapeut met een holistische blik"> <meta name="description" content="ik ben craniosacraaltherapeut met een holistische blik">',1),u=r(`<!> <div id="wsite-content" class="wsite-elements wsite-not-footer"><div class="wsite-section-wrap"><div class="wsite-section wsite-body-section wsite-background-18 wsite-custom-background"><div class="wsite-section-content"><div class="container"><div class="wsite-section-elements"><div class="paragraph"><p>Wie ben ik?</p> <p>Ik ben een paardengek. Vanaf mijn zesde iedere dag op en bij ponies te
+							vinden. Iedere week les op de ponyclub en bijna ieder weekend op wedstrijd.
+							Andere sporten vond ik gewoonweg niet interessant. Toen ik ging studeren
+							moest ik even afscheid nemen van de paarden. Ik voelde een leegte en zodra
+							ik kon zat ik weer in het zadel en pakte ik mijn sport weer vol overgave op.
+							Momenteel hebben wedstrijden plaats gemaakt voor lessen en buitenritten in
+							het bos. Iets waar ik nu samen met mijn man en onze paarden enorm van kan
+							genieten.</p> <p>Dieren hebben mij altijd aangetrokken. Van kinds af aan wilde ik dierenarts
+							worden. Maar helaas, ik bleek te slecht te zijn in de exacte vakken. En wat
+							dan, ik wist het niet. Dus daarom maar een 'verstandige studie' gekozen,
+							zodat ik in mijn vrije tijd mij kon omringen met dieren en vooral paarden.
+							Toch bleef het altijd maar knagen en was ik niet tevreden met hoe ik mij
+							leven vorm gaf. Ik besloot een duitse opleiding tot fysiotherapie (in
+							Nederland Bewegingstherapie genoemd) voor paarden te doen, omdat ik wilde
+							weten hoe het paard in elkaar zat en hoe ik daar rijkunstig mee om kon gaan.
+							En dat smaakte naar meer. Ik ging een meerjarige massageopleiding doen.
+							Hier leerde ik uiteindelijk veel meer dan alleen massage; aromatherapie,
+							werken met edelstenen, taping en raindroptherapie. Het kwam allemaal ruim
+							aan bod. Net als meerdere dissecties op de Universiteit voor
+							Diergeneeskunde.</p> <br> <strong>2023</strong><br> <a href="https://milneinstitute.com/cranial-4-c4/">Cranial 4, Developing the Inner Eye</a>, Nicola Dörffling te Hamburg, Duitsland.<br> <a href="https://blauwehengst.nl/dissectie-rug-en-achterhand/">Dissectie rug en achterhand</a>, <a href="https://blauwehengst.nl/">Blauwe Hengst</a>, Dierenkliniek <a href="https://www.delingehoeve.nl/">Lingehoeve</a> te Lienden.<br> <br> <strong>2022</strong><br> Cranial 1, The Foundation Cranial Class, Nicola Dörffling te Hamburg,
+							Duitsland.<br> Cranial 2, Coupled Holds, Sacral Work and Unwinding, Hugh Milne te Graz,
+							Oostenrijk.<br> Cranial 3, Practioner’s Skills: Sculpting the Conscious Medium, Hugh Milne
+							te Toscane, Italië.<br> <br> <strong>2021</strong><br> Craniosacral Anatomy Milne Institute, Nicola Dörffling te Hamburg,
+							Duitsland<br> <br> <strong>2020</strong><br> Equine Cranium Dissection<br> <br> <strong>2019</strong><br> Equine CranioSacral 2, Maureen Rogers<br> Equine CranioSacral 3, Maureen Rogers<br> Assistent Equine Craniosacral 1 te Minesota, Amerika<br> Equine Cranium Dissection, Maureen Rogers<br> Advanced Equine Cranium Dissection, Maureen Rogers<br> Persoonlijke leersessie, Maureen Rogers<br> <br> <strong>2018</strong><br> Praktijkdag casuïstiek, Massage Hippique<br> Whole Horse Dissection, Sharon May-Davis en Equine Studies<br> Class Headshaking, Maureen Rogers<br> Equine Assesment, Maureen Rogers<br> <br> <strong>2017</strong><br> Seminar “Comparative anatomy of Equidae” Sharon May Davis (Faculteit
+							Diergeneeskunde Utrecht)<br> Functional equine taping, Massage Hippique<br> Intuïtieve massage voor paarden L4, Massage Hippique<br> <br> <strong>2016</strong><br> Seminar “ The missing lamellae” Sharon May Davis<br> Integratieve massage voor paarden L3, Massage Hippique<br> Snijzaal Diergeneeskunde Universiteit Utrecht, voor-midden en achterhand,
+							Massage Hippique<br> <br> <strong>2015</strong><br> Sportmassage voor paarden L1 en L2, Massage Hippique<br> Equine craniosacral therapy ECS1, Maureen Rogers<br> <br> <strong>2014</strong><br> Jaaropleiding bewegingstherapeut voor paarden, ISHAS Moravita<br> <br> <br> <br></div></div></div></div></div></div></div>`,1);function M(i){var e=u();d(s=>{var o=b();l.title="Over Marion Plagge",a(6),n(s,o)});var t=g(e);p(t,{title:"OVER MARION",image:`${m??""}/images/background-images/over-mij_bg.jpg`}),a(2),n(i,e)}export{M as component};
